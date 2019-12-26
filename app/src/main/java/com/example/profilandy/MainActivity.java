46843ed700bbdf1e320228cde751b8198e50a285
andyfebrianto <56467258+andyfebrianto@users.noreply.github.com>
2019-12-26 11:27:27 +0700

    Tugas Multibahasa

--- app/src/main/java/com/example/profilandy/MainActivity.java
+++ app/src/main/java/com/example/profilandy/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.profilandy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.bahasa){
+            startActivity(new Intent(this, Vbahasa.class));
+        }
+        return true;
     }
 }
