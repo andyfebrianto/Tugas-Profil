387332b1630580899ca6bc0582f710b225369636
andyfebrianto <56467258+andyfebrianto@users.noreply.github.com>
2019-12-26 11:59:00 +0700

    Tugas Multibahasa

---
+++ app/src/main/java/com/example/profilandy/Vbahasa.java
@@ -0,0 +1,89 @@
+package com.example.profilandy;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class Vbahasa extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadlocale();
+        setContentView(R.layout.activity_bahasa);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button bahasa = findViewById(R.id.bahasa);
+        bahasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showChangeLanguageDialog();
+            }
+        });
+
+    }
+
+    private void showChangeLanguageDialog() {
+        final String[] listItems = {"Indonesia","Dayak Ngaju"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(Vbahasa.this);
+        mBuilder.setTitle("Pilih Bahasa");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i == 0){
+                    setLocale("id");
+                    recreate();
+                }
+                else if(i == 1){
+                    setLocale("fr");
+                    recreate();
+            }
+
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    public void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadlocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang","");
+        setLocale(language);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==android.R.id.home){
+            this.finish();
+            Intent a = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(a);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
